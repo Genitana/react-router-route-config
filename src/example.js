2d6900a8df5a20ba3f109f83e8cd7eee5ac725db
robert <>
2021-11-17 19:46:14 +0800

    route-config learning

--- src/example.js
+++ src/example.js
@@ -27,25 +27,60 @@ const routes = [
       }
     ]
   },
+  // {
+  //   // 这么写是有问题的，当输入/test的时候，其实匹配的是/tacos这一层，
+  //   // 所以不能把 ‘/’作为单独的一层，要把它放在最后面
+  //   path: "/", 
+  //   exact: true,
+  //   component: Test_,
+  //   routes: [
+  //     {
+  //       path: "/test",
+  //       exact: true,
+  //       component: Test,
+  //       routes: [
+  //         {
+  //           path: "/test/test1",
+  //           exact: true,
+  //           component: Test1
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // },
   {
-    path: "/",
-    exact: true,
+    // 这么写是有问题的，当输入/test的时候，其实匹配的是/tacos这一层，
+    // 所以不能把 ‘/’作为单独的一层，要把它放在最后面
+    path: "/test_", 
+    // exact: true,
     component: Test_,
     routes: [
+      /**
+       * 如果没有子路由的情况，建议大家配都加一个exact；
+       * 如果有子路由，建议在子路由中加exact，父路由不加； 
+       * 而strict是针对是否有斜杠的，一般可以忽略不配置。
+       */
       {
-        path: "/test",
-        exact: true,
+        path: "/test_/test",
+        // exact: true,
         component: Test,
         routes: [
           {
-            path: "/test/test1",
+            path: "/test_/test/test1",
             exact: true,
             component: Test1
           }
         ]
       }
     ]
-  }
+  },
+  {
+    // 这么写是有问题的，当输入/test的时候，其实匹配的是/tacos这一层，
+    // 所以不能把 ‘/’作为单独的一层，要把它放在最后面
+    path: "/", 
+    exact: true,
+    component: Root,
+  },
 ];
 
 export default function RouteConfigExample() {
@@ -61,6 +96,7 @@ export default function RouteConfigExample() {
           </li>
         </ul>
 
+        {/* 渲染第一级路由 */}
         <Switch>
           {routes.map((route, i) => (
             <RouteWithSubRoutes key={i} {...route} />
@@ -80,6 +116,7 @@ function RouteWithSubRoutes(route) {
       path={route.path}
       render={(props) => (
         // pass the sub-routes down to keep nesting
+        // route.routes 就是子路由
         <route.component {...props} routes={route.routes} />
       )}
     />
@@ -91,6 +128,8 @@ function Sandwiches() {
 }
 
 function Tacos({ routes }) {
+  // routes是传下来的子路由，这里就是 /tacos 下面的routes
+  console.log('Tacos({ routes })', routes);
   return (
     <div>
       <h2>Tacos</h2>
@@ -120,12 +159,44 @@ function Cart() {
   return <h3>Cart</h3>;
 }
 
-function Test_() {
-  return <h3>Test_</h3>;
+function Test_( routesss ) {
+  //TODO: 注意，要解构一下
+  console.log('Test_(routes)', routesss);
+  
+  // routes是它的子路由
+ const { routes } = routesss;
+ 
+  return (
+      <div>
+        <h3>Test_</h3>
+        {routes && (
+        <Switch>
+          {routes.map((route, i) => (
+            <RouteWithSubRoutes key={i} {...route} />
+          ))}
+        </Switch>)}
+        
+      </div>)
 }
-function Test() {
-  return <h3>Test</h3>;
+function Test({routes}) {
+  // 注意：要解构一下
+
+  console.log('Test({routes})', routes);
+  return (
+    <div>
+      <h3>Test</h3>
+      {routes && <Switch>
+          {routes.map((route, i) => (
+            <RouteWithSubRoutes key={i} {...route} />
+          ))}
+        </Switch>}
+      
+    </div>
+  )
 }
 function Test1() {
   return <h3>Test1</h3>;
+}
+function Root() {
+  return <h3>Root</h3>;
 }
